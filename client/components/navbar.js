62e032767c56beb6494c64bae149d8a0b1c708f2
Sam Fortuna <saf1119@gmail.com>
2018-09-28 15:38:37 -0500

    link-account

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -25,7 +25,6 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/onboarding">Sign Up</Link>
-        </div>
           <Link to="/signup">Sign Up</Link>
         </Menu.Menu>
       )}
