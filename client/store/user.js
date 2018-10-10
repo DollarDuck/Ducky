61f9cde4c4874d99792fcee25a976b97aee748b1
Stasha Yancho <stasha.yancho@gmail.com>
2018-10-10 14:10:38 -0500

    bug fix

--- client/store/user.js
+++ client/store/user.js
@@ -42,7 +42,7 @@ export const auth = (email, password, method) => async dispatch => {
 
   try {
     dispatch(getUser(res.data))
-    history.push('/me')
+    history.push(`/budget/${res.data.id}`)
   } catch (dispatchOrHistoryErr) {
     console.error(dispatchOrHistoryErr)
   }
